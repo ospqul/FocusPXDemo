932ecf4c8b55cc02a4079828dc970f3f4c7240a2
Qu Long <long.qu@olympus-ap.com>
2019-12-30 15:05:55 +0800

    3. Get Ascan Data

--- FPXDemo/Models/DeviceModel.cs
+++ FPXDemo/Models/DeviceModel.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -11,6 +12,10 @@ namespace FPXDemo.Models
     public class DeviceModel
     {
         public IDevice device { get; set; }
+        public IBeamSet beamSet { get; set; }
+        public IUltrasoundConfiguration ultrasoundConfiguration { get; set; }
+        public IDigitizerTechnology digitizerTechnology { get; set; }
+        public IAcquisition acquisition { get; set; }
 
         public DeviceModel()
         {
@@ -25,6 +30,88 @@ namespace FPXDemo.Models
                 return;
             }
             //MessageBox.Show("Device is Found!");
+            DownloadFirmwarePackage();
+        }
+
+        public void DownloadFirmwarePackage()
+        {
+            string packageName = "FocusPxPackage-1.3";
+            IFirmwarePackage firmwarePackage;
+            IFirmwarePackageCollection firmwarePackages = IFirmwarePackageScanner.GetFirmwarePackageCollection();
+            for (uint i=0; i<firmwarePackages.GetCount(); i++)
+            {
+                if (firmwarePackages.GetFirmwarePackage(i).GetName().Contains(packageName))
+                {
+                    firmwarePackage = firmwarePackages.GetFirmwarePackage(i);
+                    device.Start(firmwarePackage);
+                    break;
+                }
+            }
+        }
+
+        public void CreatBeamSet()
+        {
+            IDeviceConfiguration deviceConfiguration = device.GetConfiguration();
+            ultrasoundConfiguration = deviceConfiguration.GetUltrasoundConfiguration();
+            digitizerTechnology = ultrasoundConfiguration.GetDigitizerTechnology(UltrasoundTechnology.Conventional);
+            IBeamSetFactory beamSetFactory = digitizerTechnology.GetBeamSetFactory();
+            beamSet = beamSetFactory.CreateBeamSetConventional("Conventional");
+        }
+
+        public void BindConnector()
+        {
+            // Create a connetor at P1/R1 with index 4
+            IConnector connector = digitizerTechnology.GetConnectorCollection().GetConnector(4);
+            ultrasoundConfiguration.GetFiringBeamSetCollection().Add(beamSet, connector);
+        }
+
+        public void InitiateAcquisition()
+        {
+            if (device == null)
+            {
+                return;
+            }
+            acquisition = IAcquisition.CreateEx(device);
+        }
+
+        public ICycleData CollectCycleData()
+        {
+            if (acquisition == null)
+            {
+                return null;
+            }
+
+            var result = acquisition.WaitForDataEx();
+            if (result.status == IAcquisition.WaitForDataResultEx.Status.DataAvailable)
+            {
+                return result.cycleData;
+            }
+
+            return null;
+        }
+
+        public int[] CollectAscanData()
+        {
+            if (acquisition == null)
+            {
+                return null;
+            }
+
+            var result = acquisition.WaitForDataEx();
+            if (result.status == IAcquisition.WaitForDataResultEx.Status.DataAvailable)
+            {
+                var cycleData = result.cycleData;
+                var ascan = cycleData.GetAscanCollection().GetAscan(0);
+                int[] ascanData = new int[ascan.GetSampleQuantity()];
+
+                for (int i=0; i<ascan.GetSampleQuantity(); i++)
+                {
+                    ascanData[i] = (int)Marshal.ReadInt32(ascan.GetData(), i * 4);
+                }
+                return ascanData;
+            }
+
+            return null;
         }
     }
 }
--- FPXDemo/ViewModels/ShellViewModel.cs
+++ FPXDemo/ViewModels/ShellViewModel.cs
@@ -12,6 +12,13 @@ namespace FPXDemo.ViewModels
     {
         public DeviceModel deviceModel { get; set; }
 
+        private string _serialNumber;
+        private string _ipAddress;
+        private string _beamSetName;
+
+        private string _logging;
+
+
         public void ConnectDevice()
         {
             deviceModel = new DeviceModel();
@@ -19,7 +26,57 @@ namespace FPXDemo.ViewModels
             IPAddress = deviceModel.device.GetInfo().GetAddressIPv4();
         }
 
-        private string _serialNumber;
+        public void CreateBeamSet()
+        {
+            deviceModel.CreatBeamSet();
+            BeamSetName = deviceModel.beamSet.GetName();
+            Logging += "BeamSet is created!" + Environment.NewLine;
+        }
+
+        public void BindConnector()
+        {
+            deviceModel.BindConnector();
+            Logging += "Connector is binded!" + Environment.NewLine;
+        }
+
+        public void InitAcquisition()
+        {
+            deviceModel.InitiateAcquisition();
+            Logging += "Init Acquisition!" + Environment.NewLine;
+        }
+
+        public void CollectCycleData()
+        {
+            int number = 10;
+
+            deviceModel.acquisition.ApplyConfiguration();
+            deviceModel.acquisition.Start();
+
+            for (int i=0; i<number; i++)
+            {
+                var cycleData = deviceModel.CollectCycleData();
+                Logging += "Cycle ID: " + cycleData.GetCycleId().ToString() + Environment.NewLine;
+            }
+
+            deviceModel.acquisition.Stop();
+        }
+
+        public void CollectAscanData()
+        {
+            deviceModel.acquisition.ApplyConfiguration();
+            deviceModel.acquisition.Start();
+
+            int[] ascanData = deviceModel.CollectAscanData();
+
+            for (int i=0; i<ascanData.GetLength(0); i++)
+            {
+                Logging += ascanData[i].ToString() + ",";
+            }
+
+            Logging += Environment.NewLine;
+
+            deviceModel.acquisition.Stop();
+        }
 
         public string SerialNumber
         {
@@ -31,8 +88,6 @@ namespace FPXDemo.ViewModels
             }
         }
 
-        private string _ipAddress;
-
         public string IPAddress
         {
             get { return _ipAddress; }
@@ -43,6 +98,25 @@ namespace FPXDemo.ViewModels
             }
         }
 
+        public string BeamSetName
+        {
+            get { return _beamSetName; }
+            set
+            {
+                _beamSetName = value;
+                NotifyOfPropertyChange(() => BeamSetName);
+            }
+        }
+
+        public string Logging
+        {
+            get { return _logging; }
+            set
+            {
+                _logging = value;
+                NotifyOfPropertyChange(() => Logging);
+            }
+        }
 
 
 

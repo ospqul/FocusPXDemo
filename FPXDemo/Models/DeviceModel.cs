bc74f73cd50aca8695ef73fb6459a58d7fd2444f
Qu Long <long.qu@olympus-ap.com>
2019-12-30 12:47:32 +0800

    2. Add NET lib and Connect Device

---
+++ FPXDemo/Models/DeviceModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using OlympusNDT.Instrumentation.NET;
+
+namespace FPXDemo.Models
+{
+    public class DeviceModel
+    {
+        public IDevice device { get; set; }
+
+        public DeviceModel()
+        {
+            Utilities.ResolveDependenciesPath();
+            int timeout = 5000;
+            IDeviceDiscovery deviceDiscovery = IDeviceDiscovery.Create("192.168.0.1");
+            DiscoverResult discoverResult = deviceDiscovery.DiscoverFor(timeout);
+            device = discoverResult.device;
+            if (discoverResult.status != DiscoverResult.Status.DeviceFound)
+            {
+                MessageBox.Show("Device is not Found!");
+                return;
+            }
+            //MessageBox.Show("Device is Found!");
+        }
+    }
+}
--- FPXDemo/ViewModels/ShellViewModel.cs
+++ FPXDemo/ViewModels/ShellViewModel.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Caliburn.Micro;
+using FPXDemo.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,43 @@ using System.Threading.Tasks;
 
 namespace FPXDemo.ViewModels
 {
-    class ShellViewModel
+    class ShellViewModel : Screen
     {
+        public DeviceModel deviceModel { get; set; }
+
+        public void ConnectDevice()
+        {
+            deviceModel = new DeviceModel();
+            SerialNumber = deviceModel.device.GetInfo().GetSerialNumber();
+            IPAddress = deviceModel.device.GetInfo().GetAddressIPv4();
+        }
+
+        private string _serialNumber;
+
+        public string SerialNumber
+        {
+            get { return _serialNumber; }
+            set
+            {
+                _serialNumber = value;
+                NotifyOfPropertyChange(() => SerialNumber);
+            }
+        }
+
+        private string _ipAddress;
+
+        public string IPAddress
+        {
+            get { return _ipAddress; }
+            set
+            {
+                _ipAddress = value;
+                NotifyOfPropertyChange(() => IPAddress);
+            }
+        }
+
+
+
+
     }
 }
